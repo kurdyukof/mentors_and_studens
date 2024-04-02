841238dc588b921143101c8a0a043a9f9370b051
Igor KurdyukoV <kurdyukoff2011@ya.ru>
2024-04-02 14:43:14 +0300

    preditog

--- test 1.py
+++ test 1.py
@@ -1,49 +1,52 @@
 class Student:
-    def __init__(self, name, surname, gender):
+    def __init__(self, name, surname):
+        self.l_courses_attached = []
         self.name = name
         self.surname = surname
-        self.gender = gender
         self.finished_courses = []
         self.courses = []
         self.courses_in_progress = []
         self.grades = {}
         self.courses_attached = []
 
-    def rate_bl(self, lecturer, l_course, grade):
+    def rate_bl(self, lecturer, l_course, l_grade, ):
         if isinstance(lecturer,
-                      Lecturer) and l_course in self.courses_attached and l_course in lecturer.l_courses_in_progress:
-            if l_course in lecturer.grades:
-                lecturer.grades[l_course] += [grade]
+                      Lecturer) and l_course in self.l_courses_attached and l_course in lecturer.courses_in_progress:
+            if l_course in lecturer.l_grades:
+                lecturer.l_grades[l_course] += [l_grade]
             else:
-                lecturer.grades[l_course] = [grade]
+                lecturer.l_grades[l_course] = [l_grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return (f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.grades}\nКурсы "
+                f"в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}")
 
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.courses_attached = []
 
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        self.l_courses = []
         self.l_courses_attached = []
-        self.courses_attached = []
-        self.grades = {}
+        self.l_grades = {}
         self.lecturers = Lecturer
+        self.l_courses_in_progress = []
+
+    def __str__(self):
+        return f"Имя:{self.name}\nФамилия:{self.surname}\nСредняя оценка за лекции: {self.l_grades}"
 
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.grades = {}
-        self.finished_courses = []
-        self.courses_in_progress = []
         self.courses_attached = []
-        self.course = []
 
     def rate_nw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -54,8 +57,11 @@ class Reviewer(Mentor):
         else:
             return "Ошибка!"
 
+    def __str__(self):
+        return f"Имя:{self.name},\nФамилия: {self.surname}"
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+
+best_student = Student('Ruoy', 'Eman')
 best_student.courses_in_progress += ['Python']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
@@ -68,9 +74,18 @@ cool_reviewer.rate_nw(best_student, 'Python', 10)
 best_lecturer = Lecturer('Chaki', 'Dools')
 best_lecturer.l_courses_attached += ['Git']
 
-student.rate_bl(best_lecturer, 'Git', 15)
-cools_student.rate_bl(best_lecturer, 'Git', 15)
-cools_student.rate_bl(best_lecturer, 'Git', 15)
+cool_student = Student('Some', 'Buddy')
 
+cool_student.rate_bl(best_lecturer, 'Git', 15)
+cool_student.rate_bl(best_lecturer, 'Git', 15)
+cool_student.rate_bl(best_lecturer, 'Git', 15)
+
+some_reviewer = Reviewer('Some', 'Boddy')
+some_lecturer = Lecturer('Some', 'Boddy')
+some_student = Student('Ruoy', 'Eman')
+
+print(some_student)
 print(best_student.grades)
-print(best_lecturer.grades)
+print(best_lecturer.l_grades)
+print(some_reviewer)
+print(some_lecturer)
